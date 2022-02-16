0db83497558756119e39b73427a5fe443760eff2
Anton Galich <inthesky133@gmail.com>
2022-02-16 20:34:06 +0300

    rebaseV-02/V-03 after rebase

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,8 +3,10 @@ import classes from "./Main.module.scss";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
 
 const Main = () => {
+  const navigate = useNavigate();
   return (
     <div className={classes.main}>
       <Header />
@@ -13,8 +15,11 @@ const Main = () => {
         <h1 className={classes.title}>Каршеринг</h1>
         <h2 className={classes.subtitle}>Need for drive</h2>
         <p className={classes.descr}>Поминутная аренда авто твоего города</p>
-
-        <Button title={"Забронировать"} className={classes.button} />
+        <Button
+          onClick={() => navigate("/order/location")}
+          title={"Забронировать"}
+          className={classes.button}
+        />
       </main>
       <Footer />
     </div>
--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "../../components/Header/Header";
 import SideBar from "../../components/SideBar/SideBar";
-import Modal from "../../components/Modal/Modal";
 import { Outlet } from "react-router-dom";
 import classes from "./OrderPage.module.scss";
 import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
 import OrderForm from "../../components/Steps/OrderForm/OrderForm";
 import { IOrderItem } from "../../components/Steps/OrderForm/OrderItem/OrderItem";
+import BurgerMenu from "../../components/BurgerMenu/BurgerMenu";
 const OrderPage = () => {
   const orderItem: IOrderItem[] = [{ title: "Пункт", info: "Санкт-Петербург" }];
   return (
@@ -25,7 +25,7 @@ const OrderPage = () => {
         </div>
       </div>
 
-      <Modal />
+      <BurgerMenu />
     </section>
   );
 };
