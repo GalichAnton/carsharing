6877d8a5cef295d5983ba1b0fe244226ce3a8466
Anton Galich <inthesky133@gmail.com>
2022-02-16 13:53:35 +0300

    rebase RebaseV-02/V-03 start adaptation order page

--- src/components/Steps/Location/Location.tsx
+++ src/components/Steps/Location/Location.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import LocationForm from "./LocationForm/LocationForm";
 import classes from "./Location.module.scss";
-import OrderForm from "../OrderForm/OrderForm";
-import { IOrderItem } from "../OrderForm/OrderItem/OrderItem";
 import Map from "./Map/Map";
 const Location = () => {
-  const orderItem: IOrderItem[] = [{ title: "Пункт", info: "Санкт-Петербург" }];
   return (
     <section className={classes.location}>
       <div className={classes.container}>
@@ -13,7 +10,6 @@ const Location = () => {
         <h3 className={classes.title}>Выбрать на карте:</h3>
         <Map />
       </div>
-      <OrderForm orderItems={orderItem} startPrice={5000} endPrice={10000} />
     </section>
   );
 };
--- src/pages/OrderPage/OrderPage.tsx
+++ src/pages/OrderPage/OrderPage.tsx
@@ -5,15 +5,26 @@ import Modal from "../../components/Modal/Modal";
 import { Outlet } from "react-router-dom";
 import classes from "./OrderPage.module.scss";
 import BreadCrumbs from "../../components/BreadCrumbs/BreadCrumbs";
+import OrderForm from "../../components/Steps/OrderForm/OrderForm";
+import { IOrderItem } from "../../components/Steps/OrderForm/OrderItem/OrderItem";
 const OrderPage = () => {
+  const orderItem: IOrderItem[] = [{ title: "Пункт", info: "Санкт-Петербург" }];
   return (
     <section className={classes.container}>
       <SideBar />
       <div className={classes.content}>
         <Header />
         <BreadCrumbs />
-        <Outlet />
+        <div className={classes.wrapper}>
+          <Outlet />
+          <OrderForm
+            orderItems={orderItem}
+            startPrice={5000}
+            endPrice={10000}
+          />
+        </div>
       </div>
+
       <Modal />
     </section>
   );
